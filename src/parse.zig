600b695b69ebb3386c626464f48620ab131925d4
ominitay <37453713+ominitay@users.noreply.github.com>
2022-04-27 11:29:44 +0100

    Merge consecutive instructions at parse stage
    This should reduce the amount of time we spend at optimisations, and
    hopefully speed up the compiler. This does not affect the Bir post-optimisation.

--- src/parse.zig
+++ src/parse.zig
@@ -1,23 +1,86 @@
 const std = @import("std");
 const Bir = @import("Bir.zig");
 
+const State = enum {
+    start,
+    right,
+    left,
+    inc,
+    dec,
+};
+
 pub fn parse(allocator: std.mem.Allocator, source: []const u8) !Bir {
     var instructions = std.MultiArrayList(Bir.Instruction){};
     try instructions.ensureTotalCapacity(allocator, source.len);
 
+    var state: State = .start;
+    var counter: u32 = 0;
+
     var i: usize = 0;
-    while (i < source.len) : (i += 1) {
-        switch (source[i]) {
-            '>' => try instructions.append(allocator, .{ .tag = .move_right, .payload = 1 }),
-            '<' => try instructions.append(allocator, .{ .tag = .move_left, .payload = 1 }),
-            '+' => try instructions.append(allocator, .{ .tag = .increment, .payload = 1 }),
-            '-' => try instructions.append(allocator, .{ .tag = .decrement, .payload = 1 }),
-            '.' => try instructions.append(allocator, .{ .tag = .output }),
-            ',' => try instructions.append(allocator, .{ .tag = .input }),
-            '[' => try instructions.append(allocator, .{ .tag = .loop_begin }),
-            ']' => try instructions.append(allocator, .{ .tag = .loop_end }),
-            else => {},
+    while (i < source.len) {
+        switch (state) {
+            .start => switch (source[i]) {
+                '>' => {
+                    state = .right;
+                    counter = 1;
+                },
+                '<' => {
+                    state = .left;
+                    counter = 1;
+                },
+                '+' => {
+                    state = .inc;
+                    counter = 1;
+                },
+                '-' => {
+                    state = .dec;
+                    counter = 1;
+                },
+                '.' => try instructions.append(allocator, .{ .tag = .output }),
+                ',' => try instructions.append(allocator, .{ .tag = .input }),
+                '[' => try instructions.append(allocator, .{ .tag = .loop_begin }),
+                ']' => try instructions.append(allocator, .{ .tag = .loop_end }),
+                else => {},
+            },
+            .right => switch (source[i]) {
+                '>' => counter += 1,
+                ' ', '\n' => {},
+                else => {
+                    state = .start;
+                    try instructions.append(allocator, .{ .tag = .move_right, .payload = counter });
+                    continue;
+                },
+            },
+            .left => switch (source[i]) {
+                '<' => counter += 1,
+                ' ', '\n' => {},
+                else => {
+                    state = .start;
+                    try instructions.append(allocator, .{ .tag = .move_left, .payload = counter });
+                    continue;
+                },
+            },
+            .inc => switch (source[i]) {
+                '+' => counter += 1,
+                ' ', '\n' => {},
+                else => {
+                    state = .start;
+                    try instructions.append(allocator, .{ .tag = .increment, .payload = counter });
+                    continue;
+                },
+            },
+            .dec => switch (source[i]) {
+                '-' => counter += 1,
+                ' ', '\n' => {},
+                else => {
+                    state = .start;
+                    try instructions.append(allocator, .{ .tag = .decrement, .payload = counter });
+                    continue;
+                },
+            },
         }
+
+        i += 1;
     }
 
     instructions.shrinkAndFree(allocator, instructions.len);
