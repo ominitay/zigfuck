328dd85c4a6f718debeb2c87262b3c96ec1af877
ominitay <37453713+ominitay@users.noreply.github.com>
2022-04-27 12:02:26 +0100

    Remove instruction-merging optimisation step
    This code was very slow, and has been replaced by modifications to the
    parser. Opposite instructions won't be merged, but I don't see where we
    would need this anyway.

--- src/Bir.zig
+++ src/Bir.zig
@@ -39,45 +39,5 @@ allocator: std.mem.Allocator,
 
 pub fn optimise(self: *Self) !void {
     defer self.instructions.shrinkAndFree(self.allocator, self.instructions.len);
-    self.combine();
     // TODO: Implement optimisations!
-}
-
-// Combines consecutive instructions where possible
-fn combine(self: *Self) void {
-    var previous_index: usize = 0;
-    var i: usize = 1;
-    while (i < self.instructions.len) {
-        var previous = self.instructions.get(previous_index);
-        var current = self.instructions.get(i);
-        if ((previous.tag.isMove() and current.tag.isMove()) or (previous.tag.isIncOrDec() and current.tag.isIncOrDec())) {
-            if (previous.tag == current.tag) {
-                previous.payload += current.payload;
-                self.instructions.set(previous_index, previous);
-                _ = self.instructions.orderedRemove(i);
-            } else {
-                if (previous.payload > current.payload) {
-                    previous.payload -= current.payload;
-                    self.instructions.set(previous_index, previous);
-                    _ = self.instructions.orderedRemove(i);
-                } else if (previous.payload < current.payload) {
-                    current.payload -= previous.payload;
-                    self.instructions.set(previous_index, current);
-                    _ = self.instructions.orderedRemove(i);
-                } else { // We remove both the previous and next instructions, as they cancel each other out
-                    _ = self.instructions.orderedRemove(i);
-                    _ = self.instructions.orderedRemove(previous_index);
-                    i -= 1;
-                    if (self.instructions.len < 2) break;
-                    if (i == 0) i = 1;
-                    previous_index = i - 1;
-                }
-            }
-
-            continue;
-        }
-
-        previous_index = i;
-        i += 1;
-    }
 }
