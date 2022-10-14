049d4ef919c2cfcdb97cd5c7c91abc232494fe5c
ominitay <37453713+ominitay@users.noreply.github.com>
2022-10-14 22:39:58 +0100

    Add basic argument parsing

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,29 @@
 const std = @import("std");
+const args = @import("args");
+
 const parse = @import("parse.zig").parse;
 const codegen = @import("codegen.zig");
 const elf = @import("elf.zig");
 
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var args = try std.process.argsWithAllocator(allocator);
-    defer args.deinit();
+    const options = try args.parseForCurrentProcess(struct {
+        output: ?[]const u8 = null,
+        @"verbose-bir": bool = false,
 
-    _ = args.skip(); // own path
-    const source_path = args.next().?;
-    const out_path = args.next().?;
-    std.debug.assert(args.skip() == false);
+        pub const shorthands = .{
+            .o = "output",
+        };
+    }, allocator, .print);
+    defer options.deinit();
 
+    const source_path = options.positionals[0];
+    const out_path = options.options.output;
     const output_dir = std.fs.cwd();
 
     var source_file = try std.fs.cwd().openFile(source_path, .{});
@@ -27,7 +35,15 @@ pub fn main() !void {
     var bir = try parse(allocator, source_bytes);
     defer bir.deinit();
 
+    if (options.options.@"verbose-bir") try bir.print(stdout);
+
     try bir.optimise();
+
+
+    if (options.options.@"verbose-bir") {
+        try stdout.writeAll("Post-optimisation:\n");
+        try bir.print(stdout);
+    }
 
     try codegen.x64.generate(allocator, output_dir, out_path, bir.slice());
 }
